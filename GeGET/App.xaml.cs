4776fe2dda7905f0371ac782b8161925b4fe822d
Alexandre Hansen <eng.alexandreh@gmail.com>
2019-07-31 07:13:19 -0300

    Added Prism

--- GeGET/App.xaml.cs
+++ GeGET/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using GeGET.Infrastructure;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Unity;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -12,8 +16,18 @@ namespace GeGET
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<Login>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             this.Dispatcher.UnhandledException += OnDispatcherUnhandledException;
@@ -27,5 +41,17 @@ namespace GeGET
             // for quick debugging etc.
             e.Handled = true;
         }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            var moduleInfrastructureType = typeof(InfrastructureModule);
+            moduleCatalog.AddModule(new ModuleInfo()
+            {
+                ModuleName = moduleInfrastructureType.Name,
+                ModuleType = moduleInfrastructureType.AssemblyQualifiedName,
+                InitializationMode = InitializationMode.OnDemand
+            });
+        }
+
     }
 }
---
+++ GeGET/ViewModels/LayoutViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Commands;
+using Prism.Modularity;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeGET.ViewModels
+{
+    class LayoutViewModel : BindableBase
+    {
+        private static IModuleManager _moduleManager;
+        public DelegateCommand<string> NavigateCommand { get; set; }
+        public LayoutViewModel(IModuleManager moduleManager)
+        {
+            _moduleManager = moduleManager;
+            _moduleManager.LoadModule("InfrastructureModule");
+        }
+    }
+}
