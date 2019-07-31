4b4b14eb8f71515c35f10bb63ba28d85fafc9d73
Alexandre Hansen <eng.alexandreh@gmail.com>
2019-07-31 07:13:19 -0300

    Added Prism

---
+++ GeGET.Infrastructure/InfrastructureModule.cs
@@ -0,0 +1,28 @@
+﻿using GeGET.Infrastructure.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeGET.Infrastructure
+{
+    public class InfrastructureModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            var regionManager = containerProvider.Resolve<IRegionManager>();
+            regionManager.RegisterViewWithRegion("Header", typeof(Header));
+            regionManager.RegisterViewWithRegion("Navigation", typeof(Navigation));
+            regionManager.RegisterViewWithRegion("Main", typeof(Main));
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
